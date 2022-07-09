926cdaa06d11a9970e1001337fa754e14247f0f3
Zetta Eric Julianto <algonacci1@gmail.com>
2022-07-09 23:12:12 +0700

    feat: Add json route

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
   return 'Hello World!'
+
+@app.route('/json')
+def json():
+    json = {
+        'message': 'Hello World!'
+    }
+    return jsonify(json)
 
 if __name__ == '__main__':
   app.run()
